d41f8c360d6a348b989445eff9a6956c557eca07
Leandro Perversi <lperversi@MacBook-Air-de-Leandro.local>
2022-10-19 09:25:45 -0300

    Se agregó Class Based Views. Forms y Herencias

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from ejemplo.models import Familiar #renderiza 
+from django.shortcuts import render
+from ejemplo.models import Familiar
+from ejemplo.forms import Buscar # <--- NUEVO IMPORT
+from django.views import View # <-- NUEVO IMPORT 
 
 def index(request):
     return render(request, "ejemplo/saludar.html",{'nombre':'Leandro',
@@ -19,3 +23,24 @@ def imc(request,peso,altura):
 def monstrar_familiares(request):
   lista_familiares = Familiar.objects.all() #le dice al modelo que todas las ocurrencias de un familiar en la BD las traiga en una lista
   return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+
+class BuscarFamiliar(View):
+
+    form_class = Buscar
+    template_name = 'ejemplo/buscar.html'
+    initial = {"nombre":""}
+
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("nombre")
+            lista_familiares = Familiar.objects.filter(nombre__icontains=nombre).all() 
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'lista_familiares':lista_familiares})
+
+        return render(request, self.template_name, {"form": form})
